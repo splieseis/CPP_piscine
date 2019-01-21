e9f9abb8dca8a659a26d7f390bd76aacb4bcd151
splieseis <s.plieseis@hotmail.com>
2019-01-21 19:54:55 +0100

    v 0.1

---
+++ d00/ex00/megaphone.cpp
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: spliesei <spliesei@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/01/21 19:25:35 by spliesei          #+#    #+#             */
+/*   Updated: 2019/01/21 19:45:29 by spliesei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <cctype>
+#include <string>
+
+void megaphone(int argc, char **str)
+{
+  int i;
+  int j;
+
+  i = 1;
+  while (i < argc)
+  {
+    j = 0;
+    while (str[i][j])
+      {
+        std::cout << std::toupper(str[i][j]);
+        j++;
+      }
+    i++;
+  }
+}
+
+int main(int argc, char **argv)
+{
+  if (argc > 1)
+    megaphone(argc, argv);
+  else
+    std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+  std::cout << std::endl;
+  return 0;
+}
