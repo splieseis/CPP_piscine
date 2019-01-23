c43432b1243bea9ed574373622c52b4857c721a2
splieseis <s.plieseis@hotmail.com>
2019-01-23 23:32:27 +0100

    setters fix

---
+++ Pony.cpp
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Pony.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: spliesei <spliesei@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/01/23 21:58:58 by spliesei          #+#    #+#             */
+/*   Updated: 2019/01/23 14:30:34 by spliesei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Pony.hpp"
+
+Pony::Pony(void){ return ; }
+Pony::~Pony(void){ return ; }
+
+/* Setters */
+
+void Pony::setName(std::string str)
+{
+  this->_name = str;
+  return ;
+}
+
+void Pony::setType(std::string str)
+{
+  this->_type = str;
+  return ;
+}
+
+void Pony::setColor(std::string str)
+{
+  this->_color = str;
+  return ;
+}
+
+/* Getters */
+
+std::string Pony::getName(void)
+{
+  return (this->_name);
+}
+
+std::string Pony::getType(void)
+{
+  return (this->_type);
+}
+
+std::string Pony::getColor(void)
+{
+  return (this->_color);
+}
